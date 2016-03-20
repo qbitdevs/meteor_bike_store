216f1b9084e49c68bfe2ed2ea7a80047debc8222
jflores <jflores@cumplo.com>
2016-03-20 17:18:04 -0300

    arreglado al crear o actualizar la orden de compra

--- client/events/product_detail.js
+++ client/events/product_detail.js
@@ -2,7 +2,7 @@ Template.product_detail.events({
   'click #add_car': function(event) {
     event.preventDefault();
     var quantity = $('#add_product_quantity').val();
-    Meteor.call('add_product_car', Session.get('product_selected_id'), parseInt(quantity), function(error, result){
+    Meteor.call('add_product_car', Router.current().params.id, parseInt(quantity), function(error, result){
       if(error) console.log('Ocurrio algo al agregar producto a carrito');
     });
     MaterializeModal.confirm({
--- client/helpers/product_detail_helpers.js
+++
@@ -1,5 +0,0 @@
-Template.product_detail.helpers({
-  'exist_product': function(){
-    return(Products.findOne({_id: Session.get('product_selectd_id')}));
-  }
-});
